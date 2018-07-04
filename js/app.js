b9f2ff699daa81eb6d3e391e25a3cb76de2c8b0c
Daniela Küster <daniela.kuester@gmx.net>
2018-07-04 18:34:03 +0200

    Fix allFeeds variable

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
  */
 
 // The names and URLs to all of the feeds we'd like available.
-/*var allFeeds = [
+allFeeds = [
     {
         name: 'Udacity Blog',
         url: 'http://blog.udacity.com/feed'
@@ -22,8 +22,6 @@
         url: 'http://feeds.feedburner.com/udacity-linear-digressions'
     }
 ];
-*/
-var allFeeds = [];
 
 /* This function starts up our application. The Google Feed
  * Reader API is loaded asynchonously and will then call this
